bd5d21ca084581dc4b1d40725d6e2f7f6d740cf5
Tyuui568a <90586892+pp1522@users.noreply.github.com>
2026-05-12 19:40:00 +0700

    Small improvement

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,12 +3,16 @@ extends CharacterBody3D
 @onready var _camera_pivot := $CameraPivot as Node3D
 
 @export var speed := 32.0
+@export var rotation_speed := 1
 
 func _physics_process(delta: float) -> void:
 	var move_direction := Vector3.ZERO
-	move_direction.x = Input.get_action_strength("turn_right") - Input.get_action_strength("turn_left")
+	rotation.y -= (
+		(Input.get_action_strength("turn_right") - Input.get_action_strength("turn_left"))
+		* delta * rotation_speed)
+	rotation.y = wrapf(rotation.y, 0.0, deg_to_rad(360.0))
 	move_direction.z = Input.get_action_strength("backward") - Input.get_action_strength("forward")
-	move_direction = move_direction.rotated(Vector3.UP, _camera_pivot.rotation.y).normalized()
+	move_direction = move_direction.rotated(Vector3.UP, rotation.y).normalized()
 
 	velocity.x = move_direction.x * speed
 	velocity.z = move_direction.z * speed
